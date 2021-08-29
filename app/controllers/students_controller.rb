8ea62a3a21ffee55c66258571bcf6e25946947e2
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-08-28 22:15:45 -0400

    Pass all non-bonus tests.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,19 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def grades 
+    students = Student.order(grade: :desc)
+    render json: students
+  end
+  
+  def highest_grade 
+    student = Student.order(grade: :desc).limit(1)
+    render json: student
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
 end
